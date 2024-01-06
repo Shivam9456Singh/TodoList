a52fde9ae8df69996e73d9d896696594169c8baa
Shivam Kumar Singh <113454708+Shivam9456Singh@users.noreply.github.com>
2024-01-06 22:11:35 +0530

    commit

--- app/editTask/[id]/page.jsx
+++ app/editTask/[id]/page.jsx
@@ -1,5 +1,25 @@
 import EditTaskForm from "@/components/EditTaskForm";
 
-export default function EditTask() {
-    return <EditTaskForm />
+const getTaskById = async (id) => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/tasks/${id}`, { cache: 'no-store' });
+
+        if (!res.ok) {
+            throw new Error("Failed to load Task");
+
+        }
+
+        return res.json();
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export default async function EditTask({ params }) {
+    const { id } = params;
+    const { task } = await getTaskById(id);
+    const { title, description } = task;
+
+    return <EditTaskForm id={id} title={title} description={description} />
 }
--- components/EditTaskForm.jsx
+++ components/EditTaskForm.jsx
@@ -1,16 +1,62 @@
-export default function EditTaskForm() {
+"use client";
+
+import { useState } from "react"
+import { useRouter } from "next/navigation";
+
+export default function EditTaskForm({ id, title, description }) {
+
+    const [newTitle, setNewTitle] = useState(title);
+
+    const [newDescription, setNewDescription] = useState(description);
+
+    const router = useRouter();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await fetch(`http://localhost:3000/api/tasks/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    newTitle, newDescription
+                }),
+            });
+            if (!res.ok) {
+                throw new Error("Failed to update Task");
+            }
+            router.refresh();
+            router.push("/");
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    };
+
     return (
-        <div className="align-center p-5 my-0 w-full max-w-screen-lg mx-auto">
-            <form className="flex flex-col gap-3">
-                <input
-                    className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500" type="text" placeholder="Task Title"
-                />
-                <textarea
-                    className="border border-gray-300 rounded-md px-4 py-2 h-32 focus:outline-none focus:ring-2 focus:ring-green-500 resize-none" placeholder="Task Details"
-                ></textarea>
-                <button className="bg-green-500 hover:bg-green-600 focus:bg-green-600 font-bold text-white py-2 px-4 rounded-md focus:outline-none transition duration-300 ease-in-out self-center">Update Task</button>
-            </form>
-        </div>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+            <input
+                onChange={(e) => setNewTitle(e.target.value)}
+                value={newTitle}
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+                type="text"
+                placeholder="Task Title"
+            />
+
+            <input
+                onChange={(e) => setNewDescription(e.target.value)}
+                value={newDescription}
+                className="border border-gray-300 rounded-md px-4 py-2 h-32 focus:outline-none focus:ring-2 focus:ring-green-500 resize-none"
+                type="text"
+                placeholder="Task Details"
+            />
+            <button className="bg-green-500 hover:bg-green-600 focus:bg-green-600 font-bold text-white py-2 px-4 rounded-md focus:outline-none transition duration-300 ease-in-out self-center">Update Task</button>
+        </form>
+
 
 
     )
